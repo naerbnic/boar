b69bfae42ab2b67ae9aa0f042d2724004cf3ef75
Brian Chin <naerbnic@ucla.edu>
2014-09-27 15:55:13 -0700

    Cleanup of ProdState methods

--- BoarH/src/ProdState.hs
+++ BoarH/src/ProdState.hs
@@ -4,6 +4,8 @@ import           Data.Set (Set)
 import qualified Data.Set as S
 import           Grammar
 import qualified Grammar  as G
+import Control.Monad (liftM)
+import Data.Maybe (isJust)
 
 -- Helpers
 -----------
@@ -11,6 +13,11 @@ import qualified Grammar  as G
 maybeToSet :: Maybe a -> Set a
 maybeToSet Nothing = S.empty
 maybeToSet (Just a) = S.singleton a
+
+listIndexMaybe :: [a] -> Int -> Maybe a
+listIndexMaybe (a:_) 0 = Just a
+listIndexMaybe (_:r) i | i > 0 = listIndexMaybe r (i - 1)
+listIndexMaybe _ _ = Nothing
 
 -- | Represents a cursor position within a rule. Classically, this is
 -- represented with a dot at the appropriate location in a rule.
@@ -33,29 +40,28 @@ lhs (ProdState r _) = G.lhs r
 start :: Rule a -> ProdState a
 start r = ProdState r 0
 
+step :: ProdState a -> Maybe (a, ProdState a)
+step (ProdState r@(Rule _ prod) i) = do
+  a <- listIndexMaybe prod i
+  return (a, ProdState r (i + 1))
+
 -- | Returns true if the dot in the production state is at the end.
 --
 -- For example: @a -> b c .@ and @a -> .@ would both return true.
 complete :: ProdState a -> Bool
-complete (ProdState (Rule _ prod) i) = length prod == i
+complete ps = isJust (step ps)
 
 -- | Returns the 'Just' of the element just after the dot in a production state,
 -- or 'Nothing' if the production state is complete.
 --
 -- For example: @ a -> b . c d @ will return @'Just' c@.
 atPoint :: ProdState a -> Maybe a
-atPoint (ProdState (Rule _ prod) i) = index prod i
-  where
-    index (a:_) 0 = Just a
-    index (_:r) idx | idx > 0 = index r (idx - 1)
-    index _ _ = Nothing
+atPoint = liftM fst . step
 
 -- | Returns the 'Just' of the production state obtained by incrementing the
 -- dot forward one element, or 'Nothing' if the production state is complete.
 inc :: ProdState a -> Maybe (ProdState a)
-inc ps@(ProdState r i) = if complete ps
-  then Nothing
-  else Just $ ProdState r (i + 1)
+inc = liftM snd . step
 
 -- | @next a ps@ returns the 'Just' of the production state obtained by
 -- incrementing the dot forward one element if @a@ was just after the dot, or
