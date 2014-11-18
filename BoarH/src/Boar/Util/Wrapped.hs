4178c77be963c6231bac6beea0748cac040c2a08
Brian Chin <naerbnic@ucla.edu>
2014-11-17 21:57:35 -0800

    Minor change

--- BoarH/src/Boar/Util/Wrapped.hs
+++ BoarH/src/Boar/Util/Wrapped.hs
@@ -1,14 +1,10 @@
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE KindSignatures #-}
 
 module Boar.Util.Wrapped where
 
 import Prelude hiding (id, (.))
 import Control.Category
-
-import GHC.Exts (Constraint)
 
 data Wrapping a b = Wrapping
   { wrap :: a -> b
@@ -24,11 +20,3 @@ instance Category Wrapping where
   
 asWrapped :: Wrapping a b -> (b -> b) -> a -> a
 asWrapped w f = unwrap w . f . wrap w
-
-class S a where
-  type F a :: Constraint
-  
-type None a = (() :: Constraint)
-
-a :: None a => a -> a
-a = id
